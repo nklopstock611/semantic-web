fc686748bc19d96eb39579c165b2f8adeeff559f
Nicolás Klopstock Triana <77993013+nklopstock611@users.noreply.github.com>
2024-03-08 20:57:16 -0500

    update with vm changes

--- firstTask/pdf-downloader/main.py
+++ firstTask/pdf-downloader/main.py
@@ -7,17 +7,31 @@ Integrantes:
 - Nicolás Klopstock
 """
 import json
+import asyncio
 import logic as l
+import requester as r
 
 def main():
-    with open('./dict_split_2_1.json') as f:
+    with open('./dict_split_2.json') as f:
         papers = json.load(f)
 
-    for each_paper in papers:
-        # data[papers[each_paper]['title']] = papers[each_paper]['year']
+    items = list(papers.items())
+    starting_key = next((clave for clave, valor in papers.items() if valor['title'] == "A study of information seeking and retrieving, iii: Searchers, searc>
 
-        # print(l.get_pdf_link(papers[each_paper]))
-        print(l.download_pdf(papers[each_paper]))
+    i = 1
+    print(len(papers))
+    if starting_key:
+        found = False
+        for key, value in papers.items():
+            i += 1
+            if key == starting_key:
+                found = True
+                print(i)
+            if found:
+                print(f"Paper: {papers[key]['title']}")
+                print(l.download_pdf(papers[key]))
+                # print(r.test(papers[key]['title']))
 
 if __name__ == '__main__':
-    main()
+   main()
+   # asyncio.run(main())
