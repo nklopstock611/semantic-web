ad979665b7c58518c0b45bfec513e0edc68f9a27
Nicolás Klopstock Triana <77993013+nklopstock611@users.noreply.github.com>
2024-03-08 21:21:40 -0500

    update changes from vm

--- firstTask/pdf-downloader/requester.py
+++ firstTask/pdf-downloader/requester.py
@@ -6,9 +6,13 @@ with open('./credentials.json') as f:
 
 api_key = json_obj["API_KEY_SS"]
 
-headers = {'x-api-key': api_key}
+headers = {
+    'x-api-key': api_key,
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
 
-def verify_status_and_return(response, data: bool=False):
+def verify_status_and_return(response, data: int=0):
     """
     Verifies the status of the response and returns the data
     if it's a 200 status code.
@@ -16,14 +20,15 @@ def verify_status_and_return(response, data: bool=False):
     if response.status_code == 200:
         ret = json.dumps(response.json(), indent=2)
         ret = json.loads(ret)
-        if not data:
+        if data == 0:
             return ret
-        else:
+        elif data == 1:
             return ret['data']
+        elif data == 2:
+            return ret['data'][0]
     else:
         print(f"Error: {response.status_code}")
         print(json.dumps(response.json(), indent=2))
-        # return None
 
 def get_papers_from_author(author_name: tuple) -> dict:
     """
@@ -49,12 +54,33 @@ def get_papers_from_author(author_name: tuple) -> dict:
 
     return verify_status_and_return(response, True)
 
-# print(get_papers_from_author(('m', 'atiquzzaman')))
+def find_paper(paper_title: str) -> dict:
+    query_params = {'query': paper_title,
+                    "fields": "corpusId,title,year,referenceCount,citationCount,influentialCitationCount,isOpenAccess,openAccessPdf,publicationDate"
+    }
+    url = f'https://api.semanticscholar.org/graph/v1/paper/search/'
+    response = requests.get(
+        url,
+        headers=headers,
+        params=query_params
+    )
+
+    if response.status_code == 200:
+        ret = json.dumps(response.json(), indent=2)
+        ret = json.loads(ret)
+        try:
+#        print('AAAA:', ret['data'][0])
+            return ret['data'][0]
+        except KeyError:
+            print('Error: No "data" key')
+    else:
+        print(f"Error: {response.status_code}")
+        print(json.dumps(response.json(), indent=2))
 
 def get_paper(paper_id: str):
     """
     Returns the details of the paper in JSON format.
-    
+
     Paper object (relevant fields):
     {
         "paperId": "649def34f8be52c8b66281af98ae884c09aef38b",
@@ -81,28 +107,31 @@ def get_paper(paper_id: str):
 
     return verify_status_and_return(response)
 
-# print(get_paper('59a0dabcfa4e8d8fe4a7e539413b4239c9bee631'))  
-
 def get_pdf(paper_link: str):
     """
     Returns the url to the paper pdf.
     """
-    response = requests.get(paper_link)
-    return response
+    try:
+        response = requests.get(paper_link, timeout=60)
+        response.raise_for_status()
+        return response
+    except requests.exceptions.RequestException as e:
+        print(f"Error al obtener el PDF: {e}")
+        return None
 
 """
 1 - Entrar al arreglo de autores
 2 - Recorrer el arreglo de autores
 3 - Para cada autor, verificar si está el paper objetivo
     (es decir, comparar el título del JSON con los asociados al autor)
-    
+
     Caso 1 -> está el paper
         1.1 - sacar ID
         1.2 - request GET para sacar detalles del paper
         1.3 - ver si tiene el link de descarga
         1.4 - WEBSCRAPPER (o no sé):
             1.4.1 - Descargar el pdf
-    
+
     Caso 2 -> no está el paper
         1.1 - acaba el proceso y sigue al otro autor
 
